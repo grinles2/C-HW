31ebb6fe425da1f5b81a4643db2f15f16a7e1d8b
grinles <157224367+grinles2@users.noreply.github.com>
2026-09-20 12:47:41 +0100

    Add files via upload

---
+++ CLASSWORK/20-09/20-09.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+enum Status {
+    Online, Offline, Unknown
+};
+
+int main()
+{
+    Status userStatus = Status::Online;
+
+    switch (userStatus) {
+    case Online: std::cout << "User is online!\n";break;
+    case Offline: std::cout << "User is offline!\n";break;
+    case Unknown: std::cout << "User status is unknown!\n";break;
+    }
+
+    //int num1, num2;
+
+    //std::cin >> num1 >> num2;
+
+    //if (num1 == num2) {
+    //    std::cout << "Numbers are equal\n";
+    //}
+    //else if (num1 > num2) {
+    //    std::cout << "First number is bigger\n";
+    //}
+    //else {
+    //    std::cout << "Second number is bigger\n";
+    //}
+
+    // Оператори порівняння:
+    // ==, !=
+    // <, >
+    // <=, >=
+
+    //int age = 0;
+    //std::cout << "Enter your age: ";
+    //std::cin >> age;
+
+    //std::cout << (age >= 18 ? "You can vote!" : "You can't vote!") << '\n';
+
+    //int weekDay = 0;
+    //std::cout << "Enter week day number: ";
+    //std::cin >> weekDay;
+
+    //switch (weekDay) {
+    //case 1: 
+    //    std::cout << "MON\n";
+    //    break;
+    //case 2: 
+    //    std::cout << "TUE\n"; break;
+    //case 3: 
+    //    std::cout << "WED\n"; break;
+    //case 4: 
+    //    std::cout << "THU\n"; break;
+    //case 5: 
+    //    std::cout << "FRI\n"; break;
+    //case 6: 
+    //    std::cout << "SAT\n"; break;
+    //case 7: 
+    //    std::cout << "SUN\n"; break;
+    //default: std::cout << "Incorrect week day number!\n";
+    //}
+
+    double num1, num2;
+    char action;
+
+    std::cin >> num1 >> num2 >> action;
+
+    switch (action) {
+    case '+':std::cout << num1 + num2 << '\n'; break;
+    case '-':std::cout << num1 - num2 << '\n'; break;
+    case '*':std::cout << num1 * num2 << '\n'; break;
+    case '/':std::cout << num1 / num2 << '\n'; break;
+    default:std::cout << "Incorrect operation!\n";
+    }
+}
